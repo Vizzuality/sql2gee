2fef50be8005776283373141eee55000ade69732
Benjamin A. Laken <benlaken@gmail.com>
2017-01-23 11:18:08 +0100

    start of sphinx docs

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. sql2gee documentation master file, created by
+   sphinx-quickstart on Mon Jan 23 11:03:58 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sql2gee's documentation!
+===================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- tests/test_image_funcs.py
+++ tests/test_image_funcs.py
@@ -4,17 +4,18 @@ from sql2gee import SQL2GEE
 import ee
 from ee import Feature, Image, Initialize
 
-service_account = '390573081381-lm51tabsc8q8b33ik497hc66qcmbj11d@developer.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(service_account, './privatekey.pem')
-ee.Initialize(credentials)
-
-@pytest.mark.skip(reason="Needs to be initilised to pass.")
+#service_account = '390573081381-lm51tabsc8q8b33ik497hc66qcmbj11d@developer.gserviceaccount.com'
+#credentials = ee.ServiceAccountCredentials(service_account, './privatekey.pem')
+#ee.Initialize(credentials)
+#
+#@pytest.mark.skip(reason="Needs to be initilised to pass.")
 def test_retrieve_raw_ee_raster_metadata():
     """Test that basic raster metadata (in dictionary format) is returned when
     the postgis ST_METADATA() command is given.
     Notes:
         srtm90_v4 is a 90m Elevation image.
     """
+    pytest.skip("Until I can initilise on travis, skip this.")
     q = SQL2GEE("SELECT ST_METADATA(*) FROM srtm90_v4")
     ee_meta = {u'bands': [{u'crs': u'EPSG:4326',
                            u'crs_transform': [0.000833333333333,
@@ -41,9 +42,9 @@ def test_retrieve_raw_ee_raster_metadata():
 
 
 # @pytest.mark.skip(reason="Needs to be initilised to pass.")
-# def test_histogram():
-#     Initialize()
-#     sql = "SELECT ST_METADATA(*) FROM srtm90_v4"
-#     r = SQL2GEE(sql)
-#     r._ee_image_histogram
-#     return
+def test_histogram():
+    pytest.skip("Need to initilise on Travis")
+    sql = "SELECT ST_METADATA(*) FROM srtm90_v4"
+    r = SQL2GEE(sql)
+    r._ee_image_histogram
+    return
