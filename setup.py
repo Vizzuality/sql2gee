07ec4a84edbeb78ac41ce43ade5e7d70cadcdfa9
Benjamin A. Laken <benlaken@gmail.com>
2017-02-01 17:38:10 +0100

    updated version number in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sql2gee',
-      version='0.0.9',
+      version='0.0.11',
       description='Library to convert SQL-like queries into Google Earth Engine syntax, and return the responses',
       author='Benjamin Laken',
       author_email='benjamin.laken@vizzuality.com',
